04dcdc83139d76283218578c8f34d556abb95363
QuentinTollot <119617120+QuentinTollot@users.noreply.github.com>
2024-11-11 11:18:05 +0100

    Update mutation.py

--- src/mutation.py
+++ src/mutation.py
@@ -1,11 +1,13 @@
 import random
 
-def mutation(chromosomePop, popNumber, percentPopMutated, lowPercentGenesMutated, highPercentGenesMutated):
+def mutation(chromosomePop, popNumber, percentPopMutated, lowPercentGenesMutated, highPercentGenesMutated, bestPop):
     populationMutationNumber = int(percentPopMutated * popNumber)  #A % of our population will be mutated
     countMutations = 0
     newChromosomePop = chromosomePop.copy()
     while countMutations < populationMutationNumber:
         positionPop = random.randint(0, popNumber - 1)
+        while positionPop in bestPop:
+            positionPop = random.randint(0, popNumber - 1)
         chromosome = newChromosomePop[positionPop]
         geneNumberTransformed = random.randint(int(lowPercentGenesMutated * len(chromosome)), int(highPercentGenesMutated * len(chromosome)))  # Between X and Y % of genes mutated
         specificTransformedGenes = [random.randint(0, len(chromosome) - 1) for i in range(geneNumberTransformed)]  # Select genes to mutate
