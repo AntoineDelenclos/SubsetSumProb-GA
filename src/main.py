b9d71148fe87ac0c2c78b0846eee474a8155a537
Antoine Delenclos <antoine.delenclos37@gmail.com>
2024-11-11 18:59:21 +0100

    Using multiprocessing for each files of a directory to speed up global compute speed

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from src.solutions import folderSolutions
+import os
 
 def main():
     # Training Settings
-    POPULATION_NUMBER = 1000
+    POPULATION_NUMBER = 30000
     PERCENT_POP_MUTATED = 0.4
     LOW_PERCENT_GENES_MUTATED = 0.05
     HIGH_PERCENT_GENES_MUTATED = 0.2
@@ -10,13 +11,20 @@ def main():
     LOW_PERCENT_CUT_NUMBER = 0.02
     HIGH_PERCENT_CUT_NUMBER = 0.8
     BEST_POP_LENGTH = 100
+    CPU_CORES = os.cpu_count() #Usefull for multiprocessing
 
-    smallSolutions = folderSolutions("C:/Users/antoi/PycharmProjects/SubsetSumProb-GA/data/small/", POPULATION_NUMBER, PERCENT_POP_MUTATED, LOW_PERCENT_GENES_MUTATED, HIGH_PERCENT_GENES_MUTATED, PERCENT_POP_CROSSING, LOW_PERCENT_CUT_NUMBER, HIGH_PERCENT_CUT_NUMBER, BEST_POP_LENGTH)
-    #mediumSolutions = folderSolutions("C:/Users/antoi/PycharmProjects/SubsetSumProb-GA/data/medium/")
-    #largeSolutions = folderSolutions("C:/Users/antoi/PycharmProjects/SubsetSumProb-GA/data/large/")
-    #xlargeSolutions = folderSolutions("C:/Users/antoi/PycharmProjects/SubsetSumProb-GA/data/xlarge/")
+    #smallSolutions = folderSolutions("C:/Users/antoi/PycharmProjects/SubsetSumProb-GA/data/small/", POPULATION_NUMBER, PERCENT_POP_MUTATED, LOW_PERCENT_GENES_MUTATED, HIGH_PERCENT_GENES_MUTATED, PERCENT_POP_CROSSING, LOW_PERCENT_CUT_NUMBER, HIGH_PERCENT_CUT_NUMBER, BEST_POP_LENGTH, CPU_CORES)
+    #print(f"smallSolutions : {smallSolutions}")
 
-    print(smallSolutions)
+    #mediumSolutions = folderSolutions("C:/Users/antoi/PycharmProjects/SubsetSumProb-GA/data/medium/", POPULATION_NUMBER, PERCENT_POP_MUTATED, LOW_PERCENT_GENES_MUTATED, HIGH_PERCENT_GENES_MUTATED, PERCENT_POP_CROSSING, LOW_PERCENT_CUT_NUMBER, HIGH_PERCENT_CUT_NUMBER, BEST_POP_LENGTH, CPU_CORES)
+    #print(f"mediumSolutions : {mediumSolutions}")
+
+    largeSolutions = folderSolutions("C:/Users/antoi/PycharmProjects/SubsetSumProb-GA/data/large/", POPULATION_NUMBER, PERCENT_POP_MUTATED, LOW_PERCENT_GENES_MUTATED, HIGH_PERCENT_GENES_MUTATED, PERCENT_POP_CROSSING, LOW_PERCENT_CUT_NUMBER, HIGH_PERCENT_CUT_NUMBER, BEST_POP_LENGTH, CPU_CORES)
+    print(f"largeSolutions : {largeSolutions}")
+
+    #xlargeSolutions = folderSolutions("C:/Users/antoi/PycharmProjects/SubsetSumProb-GA/data/xlarge/", POPULATION_NUMBER, PERCENT_POP_MUTATED, LOW_PERCENT_GENES_MUTATED, HIGH_PERCENT_GENES_MUTATED, PERCENT_POP_CROSSING, LOW_PERCENT_CUT_NUMBER, HIGH_PERCENT_CUT_NUMBER, BEST_POP_LENGTH, CPU_CORES)
+    #print(f"xlargeSolutions : {xlargeSolutions}")
+
 
 if __name__ == "__main__":
     main()
--- src/solutions.py
+++ src/solutions.py
@@ -1,4 +1,5 @@
 import os
+from multiprocessing import Pool
 from src.traitement import firstTreatment, traitement, solutionList
 
 
@@ -17,10 +18,20 @@ def findSolutions(pathFile, popNumber, percentPopMutated, lowPercentGenesMutated
     print(listOfSolution)
     return listOfSolution
 
-def folderSolutions(directory_path, popNumber, percentPopMutated, lowPercentGenesMutated, highPercentGenesMutated, percentPopCrossing, lowPercentCutNumber, highPercentCutNumber, bestPopLength):
+#This will find solutions for each file (we will use multiprocessing to speed up the process)
+def folderSolutions(directory_path, popNumber, percentPopMutated, lowPercentGenesMutated, highPercentGenesMutated, percentPopCrossing, lowPercentCutNumber, highPercentCutNumber, bestPopLength, cpuCores):
     txtFiles = [os.path.join(directory_path, f) for f in os.listdir(directory_path) if f.endswith('.txt')] #list of all the .txt contained inside our folder
     filesSolutions = {}
+    numberFiles = len(txtFiles) # We will use that to know
+    print(numberFiles)
+    poolTasks = [(file, popNumber, percentPopMutated, lowPercentGenesMutated, highPercentGenesMutated, percentPopCrossing, lowPercentCutNumber, highPercentCutNumber, bestPopLength) for file in txtFiles]
+
+    with Pool(cpuCores) as pool: #This will allow for multiprocessing with all the available cores on our machine
+        testPool = pool.starmap(findSolutions, poolTasks)
+
+    count = 0
     for path in txtFiles:
-        listOfSolutions = findSolutions(path, popNumber, percentPopMutated, lowPercentGenesMutated, highPercentGenesMutated, percentPopCrossing, lowPercentCutNumber, highPercentCutNumber, bestPopLength)
-        filesSolutions.update({os.path.basename(path): listOfSolutions})
+        filesSolutions.update({os.path.basename(path): testPool[count]})
+        count += 1
+
     return filesSolutions
