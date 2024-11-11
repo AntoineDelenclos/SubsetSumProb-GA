76fc1a415332db2a54e67ea7eea231e959c70955
QuentinTollot <119617120+QuentinTollot@users.noreply.github.com>
2024-11-11 11:29:15 +0100

    Update mutation.py

--- src/mutation.py
+++ src/mutation.py
@@ -1,6 +1,9 @@
 import random
 
-def mutation(chromosomePop, popNumber, percentPopMutated, lowPercentGenesMutated, highPercentGenesMutated, bestPop):
+from src.fitnessFunction import calculate_fitness
+
+
+def mutation(chromosomePop, popNumber, percentPopMutated, lowPercentGenesMutated, highPercentGenesMutated, bestPop, A, Sa):
     populationMutationNumber = int(percentPopMutated * popNumber)  #A % of our population will be mutated
     countMutations = 0
     newChromosomePop = chromosomePop.copy()
@@ -16,4 +19,22 @@ def mutation(chromosomePop, popNumber, percentPopMutated, lowPercentGenesMutated
             chromosome[geneIndex] = (chromosome[geneIndex] + 1) % 2  # Flip the gene value between 0 and 1
         newChromosomePop[positionPop] = chromosome
         countMutations += 1
+    #we mutate the best population together
+    for positionPop in bestPop:
+        chromosome = newChromosomePop[positionPop]
+        geneNumberTransformed = random.randint(int(lowPercentGenesMutated * len(chromosome)),
+                                               int(highPercentGenesMutated * len(chromosome)))
+        specificTransformedGenes = [random.randint(0, len(chromosome) - 1) for i in range(geneNumberTransformed)]
+
+        # Apply mutation
+        mutatedChromosome = chromosome.copy()
+        for geneIndex in specificTransformedGenes:
+            mutatedChromosome[geneIndex] = (mutatedChromosome[geneIndex] + 1) % 2
+
+        # Check if mutated chromosome is better
+        original_fitness = calculate_fitness(chromosome, A, Sa)
+        mutated_fitness = calculate_fitness(mutatedChromosome, A, Sa)
+        if mutated_fitness < original_fitness:
+            newChromosomePop[positionPop] = mutatedChromosome  # Keep the mutation if it's better
+
     return newChromosomePop
